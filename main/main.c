4b989f4d32d61033516ae1e2f91e75e42411b468
Mikael Bengtsson <mikael.bengtsson@plejd.com>
2019-07-05 09:19:19 +0200

    Changed the project name. Tested and works. Need some more stuff like timers, input data from UART etc..

--- main/main.c
+++ main/main.c
@@ -41,13 +41,15 @@
 #define COLOR_DATA_LEN_MANCH (13)
 #define COLOR_DATA_LEN_SPI (COLOR_DATA_LEN_MANCH * 2)
 
+#define PIXEL_DATA_LEN_MANCH (39)
+#define PIXEL_DATA_LEN_SPI (PIXEL_DATA_LEN_MANCH * 2)
+
 #define PIXELS_NUMBER 100
 #define RGB_PACKET_LEN_SPI (39*2)
 
 #define SPI_SWAP_DATA_TX_64(data, len) __builtin_bswap64((uint64_t)data<<(64-len))
 	
-spi_device_handle_t spi;
-void *spi_tx_data_start;
+
 
 esp_err_t TLS3001_send_packet(void *data, uint32_t length);
 void TLS3001_fill_packet_cmd(uint8_t *spi_manchester_data_p, uint64_t data_in, uint32_t bit_length);
@@ -59,37 +61,43 @@ void *TLS3001_pack_packet_color(uint8_t *spi_manchester_data_p, uint64_t data_in
 void *pack_manchester_data_segment(uint8_t *spi_mem_data_p_start, uint64_t data_in, uint32_t bit_length_manch, bool last_segment_flag);
 void TLE3001_prep_color_data();
 uint64_t prep_pixel_packet(uint64_t red, uint64_t green, uint64_t blue);
-/* Can run 'make menuconfig' to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
-*/
+
+
 #define BLINK_GPIO CONFIG_BLINK_GPIO
 
+spi_device_handle_t spi;
+
+uint64_t reset_cmd;
+uint64_t synch_cmd;
+uint64_t start_cmd;
+void *spi_tx_data_start;
 
 uint64_t pixel_test = 0;
-
 uint16_t pixel1_red = 4000;
 uint16_t pixel1_green = 4;
 uint16_t pixel1_blue = 16;
 
 
-
 void blink_task(void *pvParameter)
 {
 	uint8_t *spi_tx_data_last;
-	
-	
 	pixel_test = prep_pixel_packet(4000, 4, 16);
 	
+	// Prepare the reset and synch commands
+	pack_manchester_data_segment(&reset_cmd, RESET_CMD, RESET_CMD_LEN_MANCH, true);	
+	pack_manchester_data_segment(&synch_cmd, SYNCH_CMD, SYNCH_CMD_LEN_MANCH, true);
+
+	TLE3001_prep_color_data();
 	//TLS3001_fill_packet_cmd(spi_tx_data, RESET_CMD, (RESET_CMD_LEN_BITS_SPI / 2));
 	//TLS3001_send_packet(spi_tx_data, RESET_CMD_LEN_BITS_SPI);
-	
+	TLS3001_send_packet(&reset_cmd, RESET_CMD_LEN_SPI);
 	vTaskDelay(2 / portTICK_PERIOD_MS);
-	
+	TLS3001_send_packet(&synch_cmd, SYNCH_CMD_LEN_SPI);
 	//TLS3001_fill_packet_cmd(spi_tx_data, SYNCH_CMD, (SYNCH_CMD_LEN_BITS_SPI / 2));
 	//TLS3001_send_packet(spi_tx_data, SYNCH_CMD_LEN_BITS_SPI);
 	
 	vTaskDelay(10 / portTICK_PERIOD_MS);
-	
+	TLS3001_send_packet(spi_tx_data_start, (PIXEL_DATA_LEN_SPI+START_CMD_LEN_SPI));
 	//pointer spi_tx_data points to start.
 	//spi_tx_data_last = TLS3001_pack_packet_color(spi_tx_data, START_CMD, (START_CMD_LEN_BITS_SPI / 2), false);
 	//spi_tx_data_last = TLS3001_pack_packet_color(spi_tx_data_last, pixel_test, (RGB_PACKET_LEN_SPI / 2), true);
@@ -131,73 +139,6 @@ void deinit_spi_data_buffer(void *spi_manchester_data_p)
 	heap_caps_free(spi_manchester_data_p);
 }
 
-void TLS3001_fill_packet_cmd(uint8_t *spi_manchester_data_p, uint64_t data_in, uint32_t bit_length_manch)
-{
-	static void *spi_manchester_data_last;
-	void *spi_manchester_data_start;
-
-	uint8_t byte_temp = 0;
-	uint8_t byte_last_temp = 0;
-	int32_t bits_remaining = bit_length_manch;
-
-	spi_manchester_data_start = (void*)spi_manchester_data_p;
-	
-	while (bits_remaining > 0)
-	{
-		if (bits_remaining < 4)	//Bits remaining is somewhere between 1-3.
-		{
-			byte_temp = data_in;   //No need to right shift since we are already at lowest "byte"
-			
-			byte_last_temp |= (MANCHESTER_VALUE(byte_temp & (0x01 << (bits_remaining-1))) << 6);
-			bits_remaining -= 1;
-			if (bits_remaining <= 0)
-			{
-				*spi_manchester_data_p = (uint8_t)byte_last_temp;
-				break;
-			}
-			
-			byte_last_temp |= (MANCHESTER_VALUE(byte_temp & (0x01 << (bits_remaining - 1))) << 4);
-			bits_remaining -= 1;
-			if (bits_remaining <= 0)
-			{
-				*spi_manchester_data_p = (uint8_t)byte_last_temp;
-				break;
-			}
-			
-			byte_last_temp |= (MANCHESTER_VALUE(byte_temp & (0x01 << (bits_remaining - 1))) << 2);
-			bits_remaining -= 1;
-			if (bits_remaining <= 0)
-			{
-				*spi_manchester_data_p = (uint8_t)byte_last_temp;
-				break;
-			}
-			
-			break;	//Should be nothing left here. Last 
-			
-		}
-		else
-		{
-			byte_temp = data_in >> (bits_remaining - 4);
-			*spi_manchester_data_p = (uint8_t)(
-				(MANCHESTER_VALUE(byte_temp & (0x08)) << 6) 
-				| (MANCHESTER_VALUE(byte_temp & (0x04)) << 4) 
-				| (MANCHESTER_VALUE(byte_temp & (0x02)) << 2) 
-				| (MANCHESTER_VALUE(byte_temp & (0x01)) << 0));
-		
-			
-			bits_remaining -= 4; 
-		}
-		
-		spi_manchester_data_p++;
-
-	}
-	
-	spi_manchester_data_last = (void*) spi_manchester_data_p;
-	
-	spi_manchester_data_p = (void*) spi_manchester_data_start;	//Point to start of buffer again on exit
-	//return spi_manchester_data_start;
-}
-
 void TLE3001_prep_color_data()
 {
 	uint8_t *spi_tx_data_last;
@@ -207,7 +148,7 @@ void TLE3001_prep_color_data()
 	spi_tx_data_last = pack_manchester_data_segment(spi_tx_data_start, START_CMD, START_CMD_LEN_MANCH, false);	
 	spi_tx_data_last = pack_manchester_data_segment(spi_tx_data_start, pixel1_red, COLOR_DATA_LEN_MANCH, false);
 	spi_tx_data_last = pack_manchester_data_segment(spi_tx_data_start, pixel1_green, COLOR_DATA_LEN_MANCH, false);
-	spi_tx_data_last = pack_manchester_data_segment(spi_tx_data_start, pixel1_blue, COLOR_DATA_LEN_MANCH, false);
+	spi_tx_data_last = pack_manchester_data_segment(spi_tx_data_start, pixel1_blue, COLOR_DATA_LEN_MANCH, true);
 
 } 
 
@@ -216,13 +157,14 @@ void *pack_manchester_data_segment(uint8_t *spi_mem_data_p_start, uint64_t data_
 {
 	
 	static uint8_t byte_temp = 0;
-	static uint8_t byte_pointer = 6;	
+	static uint8_t byte_pointer = 8;	
 	static uint32_t mem_p_offset = 0;
 	uint8_t *spi_mem_data_p_last = spi_mem_data_p_start + mem_p_offset;
 
 	for (size_t i = 0; i < bit_length_manch; i++)
 	{
 		//byte_pointer = (6 - ((i % 4)*2));	//byte_pointer = 6,4,2,0,6,4,2,0, ....
+		byte_pointer = byte_pointer - 2;
 		byte_temp |= (MANCHESTER_VALUE( (uint8_t)((data_in >> (bit_length_manch - i -1)) & 0x01) ) << byte_pointer);
 		
 		//i=15 (bit 3) bytepointer = 0
@@ -237,10 +179,9 @@ void *pack_manchester_data_segment(uint8_t *spi_mem_data_p_start, uint64_t data_
 			mem_p_offset ++;	//Offset-pointer variable points to next free memory
 
 			byte_temp = 0;	//reset temporary byte 
-			byte_pointer = 6;
+			byte_pointer = 8;
 		}
-
-		byte_pointer = byte_pointer - 2;
+			
 	}
 
 	if((byte_pointer != 0) && (last_segment_flag == true))
@@ -252,92 +193,13 @@ void *pack_manchester_data_segment(uint8_t *spi_mem_data_p_start, uint64_t data_
 		spi_mem_data_p_last = spi_mem_data_p_start+mem_p_offset;
 
 		byte_temp = 0;	//reset temporary byte 
-		byte_pointer = 6;
+		byte_pointer = 8;
 		mem_p_offset = 0;	//reset pointer offset
 	
 	}
 
 	//Return pointer to last data.
 	return spi_mem_data_p_last;	
-}
-
-void *TLS3001_pack_packet_color(uint8_t *spi_manchester_data_p, uint64_t data_in, uint32_t bit_length_manch, bool last_data_flag)
-{
-	uint8_t byte_temp = 0;
-	static uint64_t data_leftover = 0;
-	static uint32_t bits_leftover = 0;
-	uint8_t byte_last_temp = 0;
-	int32_t bits_remaining = 0;
-	uint64_t data = 0;
-	
-	uint8_t *spi_manchester_data_p_last = spi_manchester_data_p;
-	
-	bits_remaining = bit_length_manch + bits_leftover;
-	data = (uint64_t)(((data_leftover << (bit_length_manch + 1)) | data_in));
-	
-	
-	while (bits_remaining > 0)
-	{
-		if (bits_remaining < 4)	//Bits remaining is somewhere between 1-3.
-			{
-				data_leftover = (uint8_t)(data & 0x0F);     
-				bits_leftover = bits_remaining;
-				
-				if (last_data_flag == true)
-				{
-					byte_last_temp |= (MANCHESTER_VALUE(data_leftover & (0x01 << (bits_remaining - 1))) << 6);
-					bits_remaining -= 1;
-					if (bits_remaining <= 0)
-					{
-						*spi_manchester_data_p_last = (uint8_t)byte_last_temp;
-						spi_manchester_data_p_last++;
-						break;
-					}
-			
-					byte_last_temp |= (MANCHESTER_VALUE(data_leftover & (0x01 << (bits_remaining - 1))) << 4);
-					bits_remaining -= 1;
-					if (bits_remaining <= 0)
-					{
-						*spi_manchester_data_p_last = (uint8_t)byte_last_temp;
-						spi_manchester_data_p_last++;
-						break;
-					}
-			
-					byte_last_temp |= (MANCHESTER_VALUE(data_leftover & (0x01 << (bits_remaining - 1))) << 2);
-					bits_remaining -= 1;
-					if (bits_remaining <= 0)
-					{
-						*spi_manchester_data_p_last = (uint8_t)byte_last_temp;
-						spi_manchester_data_p_last++;
-						break;
-					}
-					
-					break;	//Should be nothing left here. Last 
-				}
-				
-				else
-				{
-					break;		//Exit loop and wait for new data.
-				}
-				
-			}
-		else
-		{
-			byte_temp = data >> (bits_remaining - 4);
-			*spi_manchester_data_p_last = (uint8_t)(
-				(MANCHESTER_VALUE(byte_temp & (0x08)) << 6) 
-				| (MANCHESTER_VALUE(byte_temp & (0x04)) << 4) 
-				| (MANCHESTER_VALUE(byte_temp & (0x02)) << 2) 
-				| (MANCHESTER_VALUE(byte_temp & (0x01)) << 0));
-		
-			
-			bits_remaining -= 4; 
-			spi_manchester_data_p_last++;
-		}
-			
-
-	}
-	return spi_manchester_data_p_last;
 }
 
 uint64_t prep_pixel_packet(uint64_t red, uint64_t green, uint64_t blue)
